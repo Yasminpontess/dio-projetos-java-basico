17de519ad1a56d9cce6fb2b6e0a19a25f854f01c
Yasminpontess <161251647+Yasminpontess@users.noreply.github.com>
2024-08-21 22:40:05 -0300

    Desafio conta banco
    Meu primeiro projeto na DIO
    
    BOOTCAMP SATANDER

---
+++ vs/ContaTerminal.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+
+		int agencia;
+		String conta = "220304-X ";
+		String nomeCliente = " ";
+		double saldo = 1.000;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Por favor digite o seu Nome completo:");
+
+		nomeCliente = scanner.nextLine();
+
+		System.out.println("Olá, " + nomeCliente + " Seja Bem-vindo !!! ");
+
+		System.out.println("Por favor, digite o numero da Agancia!");
+
+		agencia = scanner.nextInt();
+
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
+				+ agencia + ", conta " + conta + " e seu saldo é " + saldo + ". Já está disponível para saque.");
+
+	}
+
+}
